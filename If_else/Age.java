9123abed2c19060b71d92d2de9d41e27164f5061
sahil-shahare <sahilshahare380@gmail.com>
2025-08-03 16:43:13 +0530

    first commit

---
+++ If_else/Age.java
@@ -0,0 +1,15 @@
+public class Age{
+    public static void main(String[] args){
+        int age = 14;
+        
+        if(age>=18){
+            System.out.println("You are an adult.");
+        }
+        else if(age>=13 && age<18){
+            System.out.println("You are a teenager.");
+        }
+        else {
+            System.out.println("You are a minor.");
+        }
+    }
+}
