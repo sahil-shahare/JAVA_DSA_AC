a131a83aa5ef1cbe341d6dd5559e2d7fef15ef2e
sahil-shahare <sahilshahare380@gmail.com>
2025-08-04 00:03:42 +0530

    For Loop

---
+++ 3. Loops/For_loop/reverse_number.java
@@ -0,0 +1,13 @@
+public class reverse_number{
+    public static void main(String[] args) {
+        int n = 10899;
+        int reverse = 0;
+        while(n>0){
+            int lastDigit = n%10;
+            reverse = reverse * 10 + lastDigit;
+            n /= 10;
+        }
+        System.out.println("Reversed Number: " + reverse);
+        System.out.println();
+    }
+}
