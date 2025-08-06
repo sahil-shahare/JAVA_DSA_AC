a96a12efc2f2396781785a72e8faafeb884e1c6f
sahil-shahare <sahilshahare380@gmail.com>
2025-08-07 00:11:18 +0530

    Functions

--- 5. Functions/Prime.java
+++ 5. Functions/Prime.java
@@ -1,11 +1,14 @@
+
 import java.util.*;
-public class Prime{
-    public static boolean isPrime(int n){
-        if(n==2){
+
+public class Prime {
+
+    public static boolean isPrime(int n) {
+        if (n == 2) {
             return true;
         }
-        for(int i=2; i<=Math.sqrt(n); i++){
-            if(n%i==0){
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
@@ -16,7 +19,7 @@ public class Prime{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number to check if it's prime: ");
         int num = sc.nextInt();
-        if(isPrime(num)){
+        if (isPrime(num)) {
             System.out.println(num + " is a prime number.");
         } else {
             System.out.println(num + " is not a prime number.");
