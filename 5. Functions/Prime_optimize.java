f2918362398a7d1f5ee428764618470b3e6c705c
sahil-shahare <sahilshahare380@gmail.com>
2025-08-11 10:10:54 +0530

    Function

--- 5. Functions/Prime_optimize.java
+++ 5. Functions/Prime_optimize.java
@@ -1,11 +1,11 @@
 public class Prime_optimize {
     public static boolean isPrime(int n) {
-        if(n==2){
+        if (n == 2) {
             return true;
         }
 
-        for(int i = 2; i<=Math.sqrt(n);i++){
-            if(n%i==0){
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
