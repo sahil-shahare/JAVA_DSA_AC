c01b7963ae3eff5fb91b4b3c7b2d4855ff7e8907
sahil-shahare <sahilshahare380@gmail.com>
2025-08-12 11:22:19 +0530

    Patterns II

--- 6. Patterns II/Hollow_rectangle.java
+++ 6. Patterns II/Hollow_rectangle.java
@@ -1,9 +1,12 @@
 public class Hollow_rectangle {
 
     public static void hollow_rectangle(int total_rows, int total_cols) {
+        // outer lopp
         for (int i = 1; i <= total_rows; i++) {
+            // inner loop
             for (int j = 1; j <= total_cols; j++) {
-                if (i == 1 || i == total_rows || j == 1 || j == total_cols) {
+                // cell(i,j)
+                if (i == 1 || i == total_rows || j == 1 || j == total_cols) { // boundary cells
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
