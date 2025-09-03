e656768c35406a091e66c93327140b42d7e6fd81
sahil-shahare <sahilshahare380@gmail.com>
2025-09-03 22:00:46 +0530

    Add Question Sheet

--- 7. Array I/Array.java
+++ 7. Array I/Array.java
@@ -95,7 +95,7 @@ public class Array {
     public static void subArray(int number[]) {
         for (int i = 0; i < number.length; i++) {
             for (int j = i; j < number.length; j++) {
-                for (int k = i; k < j; k++) {
+                for (int k = i; k <= j; k++) {
                     System.out.print(number[k] + " ");
                 }
                 System.out.println();
