28b4ab7a5ff2d7cc3ca80ebab90d5f045d6a8bdf
sahil-shahare <sahilshahare380@gmail.com>
2025-09-03 14:34:12 +0530

    Array

--- 7. Array I/Array.java
+++ 7. Array I/Array.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Array {
 
     // Print
@@ -8,11 +10,64 @@ public class Array {
         System.out.println();
     }
 
-    // Reverse an Array
+    // LinearSearch
+    public static int linearSearch(int number[], int key) {
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void getlargestsmallest(int number[]) {
+
+        if (number == null || number.length == 0) {
+            System.out.println("Array is empty!");
+            return;
+        }
+
+        int largest = Integer.MIN_VALUE; // -Infinity
+        int smallest = Integer.MAX_VALUE; // +Infinity
+        for (int i = 0; i < number.length; i++) {
+            if (largest < number[i]) {
+                largest = number[i];
+            }
+            if (smallest > number[i]) {
+                smallest = number[i];
+            }
+        }
+        System.out.println("Largest : " + largest);
+        System.out.println("Smallest : " + smallest);
+    }
+
+    public static int binarySearch(int number[], int key) {
+        int start = 0;
+        int end = number.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2; // in int never overflows because subtraction keeps the number within
+                                                 // safe range.
+
+            if (number[mid] == key) { // Found
+                return mid;
+            }
+            if (number[mid] < key) { // right
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     public static void reverse(int number[]) {
+        if (number == null || number.length <= 1) {
+            System.out.println("Nothing to reverse");
+            return;
+        }
+
         int first = 0;
         int last = number.length - 1;
-
         while (first < last) {
             int temp = number[last];
             number[last] = number[first];
@@ -23,18 +78,20 @@ public class Array {
         print(number);
     }
 
-    // Pairs of an Array
-    public static void pairs(int number[]) {
-        for (int i = 0; i < number.length; i++) {
-            for (int j = i + 1; j < number.length; j++) {
-                System.out.print("(" + number[i] + "," + number[j] + ") ");
+    public static void printPairs(int number[]){
+        if (number == null || number.length < 2) {
+            System.out.println("No pairs possible");
+            return;
+        }
+
+        for(int i=0;i<number.length;i++){
+            for(int j=i+1;j<number.length;j++){
+                System.out.print("("+number[i]+","+number[j]+") ");
             }
             System.out.println();
         }
-        System.out.println();
     }
 
-    // SubArrays
     public static void subArray(int number[]) {
         for (int i = 0; i < number.length; i++) {
             for (int j = i; j < number.length; j++) {
@@ -47,11 +104,33 @@ public class Array {
         }
         System.out.println();
     }
-
     public static void main(String[] args) {
-        int number[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+        Scanner sc = new Scanner(System.in);
+
+        // User Input
+        // int[] arr = new int[5];
+        // for (int i = 0; i < arr.length; i++) {
+        // System.out.print("Enter Number: ");
+        // arr[i] = sc.nextInt();
+        // }
+
+        // for (int i = 0; i < arr.length; i++) {
+        // System.out.println("arr[" + i + "] = " + arr[i]);
+        // }
+
+        int[] number = { 20, 4, 6, 8, 10, 102, 14, 16, 18 };
+        int key = 16;
+        // int index = linearSearch(number, key);
+        // System.out.println("Key " + key + " at index : " + index);
+
+        // getlargestsmallest(number);
+        // binarySearch(number, key);
+        // System.out.println("Index for key " + key + " is : " + binarySearch(number,
+        // key));
+
         // reverse(number);
-        // pairs(number);
+        // printPairs(number);
         subArray(number);
+
     }
 }
