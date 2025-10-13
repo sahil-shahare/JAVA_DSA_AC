4791dae5915fcb41e6d9da363e891d031383b991
sahil-shahare <sahilshahare380@gmail.com>
2025-10-13 16:49:37 +0530

    DSA

--- 1. Sum/Sum_User_input.java
+++ 1. Sum/Sum_User_input.java
@@ -1,10 +1,11 @@
 import java.util.*;
-public class Sum_User_input{
+
+public class Sum_User_input {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int a = sc.nextInt();
-       int b = sc.nextInt();
-       int sum = a+b;
-       System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
     }
 }
