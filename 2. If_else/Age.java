1ac6cc077a8e1d12a4618fa03ec4976b1b4cde80
sahil-shahare <sahilshahare380@gmail.com>
2025-10-14 12:49:45 +0530

    DSA

--- 2. If_else/Age.java
+++ 2. If_else/Age.java
@@ -1,14 +1,12 @@
-public class Age{
-    public static void main(String[] args){
+public class Age {
+    public static void main(String[] args) {
         int age = 14;
-        
-        if(age>=18){
+
+        if (age >= 18) {
             System.out.println("You are an adult.");
-        }
-        else if(age>=13 && age<18){
+        } else if (age >= 13 && age < 18) {
             System.out.println("You are a teenager.");
-        }
-        else {
+        } else {
             System.out.println("You are a minor.");
         }
     }
