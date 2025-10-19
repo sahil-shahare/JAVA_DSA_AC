ebaf4dc810fe029134e90894ec9808139516fa02
sahil-shahare <sahilshahare380@gmail.com>
2025-10-19 11:31:38 +0530

    DSA

--- 2. If_else/Largest_3_num.java
+++ 2. If_else/Largest_3_num.java
@@ -4,13 +4,11 @@ public class Largest_3_num {
         int num2 = 20;
         int num3 = 35;
 
-        if(num1>=num2 && num1>=num3){
+        if (num1 >= num2 && num1 >= num3) {
             System.out.println("The Largest number is: " + num1);
-        }
-        else if(num2>=num1 && num2>=num3){
+        } else if (num2 >= num1 && num2 >= num3) {
             System.out.println("The Largest number is: " + num2);
-        }
-        else{
+        } else {
             System.out.println("The Largest number is : " + num3);
         }
     }
