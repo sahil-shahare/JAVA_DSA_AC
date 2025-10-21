139396c308b07784fe0a44118ca1ebd228c09414
sahil-shahare <sahilshahare380@gmail.com>
2025-10-21 11:38:53 +0530

    DSA

--- 3. Loops/For_loop/reverse_number.java
+++ 3. Loops/For_loop/reverse_number.java
@@ -1,9 +1,9 @@
-public class reverse_number{
+public class reverse_number {
     public static void main(String[] args) {
         int n = 10899;
         int reverse = 0;
-        while(n>0){
-            int lastDigit = n%10;
+        while (n > 0) {
+            int lastDigit = n % 10;
             reverse = reverse * 10 + lastDigit;
             n /= 10;
         }
