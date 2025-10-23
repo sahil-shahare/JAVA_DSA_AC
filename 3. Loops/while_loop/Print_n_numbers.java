852f80a03ebb9d76d7fc7d57bf91b0f63d026d46
sahil-shahare <sahilshahare380@gmail.com>
2025-10-23 12:52:04 +0530

    DSA

--- 3. Loops/while_loop/Print_n_numbers.java
+++ 3. Loops/while_loop/Print_n_numbers.java
@@ -1,13 +1,14 @@
 import java.util.*;
-public class Print_n_numbers{
+
+public class Print_n_numbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number:");
 
         int n = sc.nextInt();
         int i = 1;
-        while(i<=n){
-            System.out.print(i+" ");
+        while (i <= n) {
+            System.out.print(i + " ");
             i++;
         }
     }
