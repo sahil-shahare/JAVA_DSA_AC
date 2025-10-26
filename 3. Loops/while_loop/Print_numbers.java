222cae6bd70c7c4cec5b1dbcd7a675b75e1f04ea
sahil-shahare <sahilshahare380@gmail.com>
2025-10-26 12:44:06 +0530

    DSA

--- 3. Loops/while_loop/Print_numbers.java
+++ 3. Loops/while_loop/Print_numbers.java
@@ -1,7 +1,7 @@
-public class Print_numbers{
+public class Print_numbers {
     public static void main(String[] args) {
-        int i=1;
-        while(i<=10){
+        int i = 1;
+        while (i <= 10) {
             System.out.println(i);
             i++;
         }
