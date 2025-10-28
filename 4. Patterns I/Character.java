828a826ebb45453e9487dd4d465ed25d8c8a79eb
sahil-shahare <sahilshahare380@gmail.com>
2025-10-28 10:58:07 +0530

    DSA

--- 4. Patterns I/Character.java
+++ 4. Patterns I/Character.java
@@ -1,11 +1,11 @@
-public class Character{
-    public static void main(String[] args){
+public class Character {
+    public static void main(String[] args) {
         char ch = 'A';
-        int n=4;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(ch);
-                ch++; 
+                ch++;
             }
             System.out.println("");
         }
