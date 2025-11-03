95841c4ad2878c3c6da27a2f93375709cf1d04ee
sahil-shahare <sahilshahare380@gmail.com>
2025-11-03 11:25:16 +0530

    DSA

--- 4. Patterns I/Star.java
+++ 4. Patterns I/Star.java
@@ -1,7 +1,7 @@
-public class Star{
-    public static void main(String[] args){
-        for(int i=1; i<=5; i++){
-            for(int j=1; j<=i; j++){
+public class Star {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println("");
