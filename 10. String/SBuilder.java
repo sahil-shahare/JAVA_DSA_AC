2405c47c62f9d7d08a9134530e47e79acfbc0bab
sahil-shahare <sahilshahare380@gmail.com>
2025-11-16 10:53:35 +0530

    DSA

--- 10. String/SBuilder.java
+++ 10. String/SBuilder.java
@@ -19,7 +19,7 @@ public class SBuilder {
         }
         return sb.toString();
     }
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         // StringBuilder sb = new StringBuilder("");
         // for (char ch = 'a'; ch <= 'z'; ch++) {
         //     sb.append(ch + " ");
