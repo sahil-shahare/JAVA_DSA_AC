b34c3c7cc5d509a38f4a21358c91758d76099081
sahil-shahare <sahilshahare380@gmail.com>
2025-11-18 13:45:06 +0530

    DSA

--- 10. String/SBuilder.java
+++ 10. String/SBuilder.java
@@ -1,29 +1,29 @@
 public class SBuilder {
 
-    //For a given String convert each the first letter of each the first letter of each word to uppercase
-    public static String toUpperCase(String str){
+    // For a given String convert each the first letter of each the first letter of
+    // each word to uppercase
+    public static String toUpperCase(String str) {
         StringBuilder sb = new StringBuilder("");
 
-        char ch=(char) Character.toUpperCase(str.charAt(0));
+        char ch = (char) Character.toUpperCase(str.charAt(0));
         sb.append(ch);
 
-        for(int i=1;i<str.length();i++){
-            if(str.charAt(i)==' '&&i<str.length()-1){
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
                 sb.append(str.charAt(i));
                 i++;
                 sb.append(Character.toUpperCase(str.charAt(i)));
-            }
-            else{
+            } else {
                 sb.append(str.charAt(i));
             }
         }
         return sb.toString();
     }
-    
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         // StringBuilder sb = new StringBuilder("");
         // for (char ch = 'a'; ch <= 'z'; ch++) {
-        //     sb.append(ch + " ");
+        // sb.append(ch + " ");
         // }
         // System.out.println(sb);
 
