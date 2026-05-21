54d3dd44efba034f3b5b04f22675059c9fefcd29
sahil-shahare <sahilshahare380@gmail.com>
2026-05-21 11:26:56 +0530

    Array

--- 7. Array I/R1.java
+++ 7. Array I/R1.java
@@ -130,7 +130,7 @@ public class R1 {
     }
 
     public static void main(String[] args) {
-        int[] numbers = { 2, 4, 6, 8, 10, 12, 14, 16, 18,20 };
+        int[] numbers = { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
         int key = 18;
 
         int[] number1 = { -2, -3, 4, -1, -2, 1, 5, -3 };
