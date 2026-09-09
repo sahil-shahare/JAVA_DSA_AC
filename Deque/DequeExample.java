de4dd284bf53dcc7b352e525e0efed1b76d0eb52
sahil-shahare <sahilshahare380@gmail.com>
2026-09-09 21:00:46 +0530

    Learn Stack

---
+++ Deque/DequeExample.java
@@ -0,0 +1,5 @@
+public class DequeExample {
+    public static void main(String[] args) {
+        
+    }
+}
--- Stack/IsBalance.java
+++ Stack/IsBalance.java
@@ -27,5 +27,30 @@ public class IsBalance {
     public static boolean isBalanced2(String str) {
         Stack<Character> stack = new Stack<>();
 
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return false;
+                }
+                stack.pop();
+            } else if (ch == '}') {
+                if (stack.isEmpty() || stack.peek() != '{') {
+                    return false;
+                }
+                stack.pop();
+            } else if (ch == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
